d4cebc093623f654795609bf23a6dbfcb27338b0
knightfury1493 <suhaibahmmad1493@gmail.com>
2021-11-19 02:23:55 +0600

    Made the text size dynamic and integrated user input.

--- src/Vehicle.js
+++ src/Vehicle.js
@@ -1,52 +1,50 @@
-class Vehicle{
-    constructor(target)
-    {
-        this.pos = createVector(random(width),random(height));
+class Vehicle {
+    constructor(target, sw) {
+        this.sw = sw;
+        this.pos = createVector(random(width), random(height));
         // this.pos = createVector(x,y);
         this.target = target.pos;
         // this.vel = createVector(0,0);
-        this.vel = p5.Vector.random2D().mult(5) ;
-        this.acc = createVector(0,0);
-       
+        this.vel = p5.Vector.random2D().mult(5);
+        this.acc = createVector(0, 0);
+
         //Both these variables control the speed by which the particle reach their desired target/destination.
         this.maxSpeed = 6; //How big the steps will be
         this.maxForce = 2; //How fast the steps will be 
 
     }
 
-    flee()
-    {
-        let mouse = createVector(mouseX,mouseY);
+    flee() {
+        let mouse = createVector(mouseX, mouseY);
 
         // fleeVel vector from mouse towards position, that is a vector opposite the mouse pointer
-        let fleeVel = p5.Vector.sub(this.pos,mouse);
+        let fleeVel = p5.Vector.sub(this.pos, mouse);
         let dis = fleeVel.mag();
         // Setting the fleeVel vector magnitude
         fleeVel.setMag(this.maxSpeed);
 
         // vector from particle vector towards fleeVel vector
-        let fleeSteer = p5.Vector.sub(fleeVel,this.vel);
-       
+        let fleeSteer = p5.Vector.sub(fleeVel, this.vel);
+
         // if distance from mouse pointer to particle is less than 100(arbitary),apply the fleeSteer force
-        if(dis < 100){
+        if (dis < 100) {
             fleeSteer.limit(15);
             this.applyForce(fleeSteer);
         }
 
     }
 
-    arrive()
-    {
+    arrive() {
         //DesireVel is vector from particle position towards targets position.
-        let desiredVel = p5.Vector.sub(this.target,this.pos);
+        let desiredVel = p5.Vector.sub(this.target, this.pos);
 
         let dis = desiredVel.mag();
         let speed = this.maxSpeed;
 
         //Mapping the speed of desireVel based on the distance of particle from the target.
         //Chossing arbitary distance of 100
-        if(dis < 100){
-            speed = map(dis,0,100,0,this.maxSpeed);
+        if (dis < 100) {
+            speed = map(dis, 0, 100, 0, this.maxSpeed);
         }
 
         //Setting the desiredVel speed
@@ -54,7 +52,7 @@ class Vehicle{
 
         //steer is vector from particle velocity vector towards desired velocity vector.
         //Can't just go directly towards the desired velocity, otherwise particle end up revolving around the target.Centrigual force.
-        let steer = p5.Vector.sub(desiredVel,this.vel);
+        let steer = p5.Vector.sub(desiredVel, this.vel);
         steer.limit(this.maxForce);
 
         this.applyForce(steer);
@@ -62,28 +60,24 @@ class Vehicle{
 
 
 
-    applyForce(force)
-    {
+    applyForce(force) {
         this.acc.add(force);
     }
 
 
-    update()
-    {
+    update() {
         this.vel.add(this.acc);
         this.pos.add(this.vel);
-        this.acc.set(0,0);
+        this.acc.set(0, 0);
     }
 
-    show()
-    {
-        strokeWeight(7);
-        if(dist(this.target.x,this.target.y,this.pos.x,this.pos.y) < 0.1){
-            stroke(random(0,255),random(0,255),random(0,255));
-         }
-         else stroke(255);
+    show() {
+        strokeWeight(this.sw);
+        if (dist(this.target.x, this.target.y, this.pos.x, this.pos.y) < 0.1) {
+            stroke(random(0, 255), random(0, 255), random(0, 255));
+        } else stroke(255);
         fill(255);
-        point(this.pos.x,this.pos.y);
+        point(this.pos.x, this.pos.y);
     }
 
 }
--- src/sketch.js
+++ src/sketch.js
@@ -3,11 +3,16 @@
 let stop = false;
 
 //For scene 1
-let text1 = [];
 let target1 = [];
+let _text1 = "Hi!There!";
+let fontSize1 = 200;
+let sw1 = 7;
+
 //For scene 2
-let text2 = [];
 let target2 = [];
+let _text2 = "Welcome";
+let fontSize2 = 200;
+let sw2 = 7;
 
 let b = [];
 let pt;
@@ -15,104 +20,187 @@ let play = true;
 let transition = false;
 let count = 1;
 
+let temp1;
+let temp2;
+
 
 //Loading font
-function preload(){
+function preload() {
     font = loadFont('Roboto-Regular.ttf');
 }
 
 
-function setup()
-{
-    createCanvas(700,400);
+function setup() {
+
+    let cnv = createCanvas(700, 400);
+    utility.clickStop(cnv);
     colorMode(HSB);
 
+    changeInput1();
+    changeInput2();
+    let p = createP("Press space for transition.");
+    let p2 = createP("Hover over the text.");
+
+    // if (fontSize1 < 100) {
+    //     console.log("Reduce the text length");
+    //     halt();
+    // }
+
+    temp1 = getFontSize(_text1);
+    temp2 = getFontSize(_text2);
+
     //Text of the scenes, needed some work to do here.
-    //TODO: Make it interective with DOM element
     // textToPoints(), p5 method returns array of points outlining the text.
-    points1 = font.textToPoints("Hi,There!",10,250,170);
-    points2 = font.textToPoints("Welcome!",20,250,150);
+    points1 = font.textToPoints(_text1, 10, 250, temp1.fontSize);
+    points2 = font.textToPoints(_text2, 10, 250, temp2.fontSize);
+
 
 
     //Making the targent objects for scene 1
-    for(let i = 0; i < points1.length; i++)
-    {
-        stroke(255);
-        strokeWeight(7);
-        target1[i] = new Target(points1[i].x,points1[i].y);
+    for (let i = 0; i < points1.length; i++) {
+        target1[i] = new Target(points1[i].x, points1[i].y);
+        b[i] = new Vehicle(target1[i], temp1.strokeWgt);
+
     }
 
     //Making the target objects for scene 2
-    for(let i = 0; i < points2.length; i++){
-        stroke(255);
-        strokeWeight(7);
-        target2[i] = new Target(points2[i].x,points2[i].y);
-    }
-
-    //For the initial scene
-    for(let i = 0; i < target1.length; i++)
-    {
-        //Do something
-        b[i] = new Vehicle(target1[i]);
+    for (let i = 0; i < points2.length; i++) {
+        target2[i] = new Target(points2[i].x, points2[i].y);
     }
 
 }
 
-function halt(){
+function halt() {
     noLoop();
 }
 
 
 // keycode 32 is 'space'
-function keyTyped()
-{
-    if(keyCode === 32)transition = true;
+function keyTyped() {
+    if (keyCode === 32) transition = true;
 }
 
-function draw()
-{
-    if(stop){halt();}
+function draw() {
+    if (stop) { halt(); }
     background(0);
+
+
 
     //If space is pressed transition is true, target for the appropiate scene is pushed into the b array.
     // TODO: Try to do it with async 
-    if(transition){   
+    if (transition) {
 
-        if(count%2)
-        {
-            for(let i = 0; i < target2.length; i++)
-            {
-                b[i] = new Vehicle(target2[i]);
+        if (count % 2) {
+            for (let i = 0; i < target2.length; i++) {
+                b[i] = new Vehicle(target2[i], temp2.strokeWgt);
             }
             //Removing the leftover from the previous scene
-            b.splice(target2.length,b.length - target2.length);
+            b.splice(points2.length, b.length - points2.length);
 
-        }
-        else
-        {
-            for(let i = 0; i < target1.length; i++)
-            {
-                b[i] = new Vehicle(target1[i]);
+        } else {
+            for (let i = 0; i < target1.length; i++) {
+                b[i] = new Vehicle(target1[i], temp1.strokeWgt);
             }
             // Removing the leftover from the previous scene
-            b.splice(target1.length,b.length - target1.length);
+            b.splice(points1.length, b.length - points1.length);
         }
-         
+
         transition = false;
 
         count++;
 
+        // console.log(count);
+
     }
 
 
-    for(let i = 0; i < b.length; i++)
-    {
+    for (let i = 0; i < b.length; i++) {
         b[i].update();
         b[i].arrive();
         b[i].flee();
         b[i].show();
     }
+}
 
+
+
+function changeInput1() {
+    let inp = createInput(_text1);
+    inp.parent(select("#inp1"));
+    inp.size = 200;
+
+    let button1 = createButton("Change");
+    button1.parent(select("#inp1"));
+
+
+    button1.mouseClicked(() => {
+        _text1 = inp.value();
+        temp1 = getFontSize(_text1)
+        points1 = font.textToPoints(_text1, 10, 250, temp1.fontSize);
+
+        for (let i = 0; i < points1.length; i++) {
+            target1[i] = new Target(points1[i].x, points1[i].y);
+            b[i] = new Vehicle(target1[i], temp1.strokeWgt);
+
+        }
+
+        b.splice(points1.length, b.length - points1.length);
+
+        count++;
+
+        transition = false;
+
+
+    });
 
 }
 
+function changeInput2() {
+    let inp2 = createInput(_text2);
+    inp2.parent(select("#inp2"));
+    inp2.size = 200;
+
+    let button2 = createButton("Change 2");
+    button2.parent(select("#inp2"));
+
+    button2.mouseClicked(() => {
+        _text2 = inp2.value();
+        temp2 = getFontSize(_text2);
+        points2 = font.textToPoints(_text2, 10, 250, temp2.fontSize);
+        for (let i = 0; i < points2.length; i++) {
+            target2[i] = new Target(points2[i].x, points2[i].y);
+            b[i] = new Vehicle(target2[i], temp2.strokeWgt);
+
+        }
+
+        b.splice(points2.length, b.length - points2.length);
+
+        count++;
+
+        transition = false;
+
+    });
+
+}
+
+
+function getFontSize(_text, sw = 10) {
+    let fs = 300;
+    while (1) {
+        let temp = font.textBounds(_text, 10, 250, fs);
+
+        if (temp.x + temp.w > 675) {
+            fs--;
+            if (fs % 30 == 0) sw--;
+        } else {
+            break;
+        }
+    }
+
+    let obj = {
+        fontSize: fs,
+        strokeWgt: sw
+    }
+
+    return obj;
+}
